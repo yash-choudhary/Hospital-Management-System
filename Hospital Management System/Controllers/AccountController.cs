c3e44af318f77166c3f0750fd4857a9d6f652f2a
Yash Choudhary <yc1998a@gmail.com>
2021-02-20 15:11:32 +0530

    update

--- Hospital Management System/Controllers/AccountController.cs
+++ Hospital Management System/Controllers/AccountController.cs
@@ -135,13 +135,6 @@ namespace Hospital_Management_System.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    //await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
                     var patient = new Patient { FirstName = model.FirstName, LastName = model.LastName, EmailAddress = model.Email, ApplicationUserId = user.Id };
                     db.Patients.Add(patient);
                     db.SaveChanges();
